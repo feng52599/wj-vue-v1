2293202f93b21e9f562b0e3375f6b852c71c3095
feng52599 <1143874188@qq.com>
2020-05-26 18:21:39 +0800

    文章管理功能全部能够使用

--- src/router/index.js
+++ src/router/index.js
@@ -31,7 +31,7 @@ export default new Router({
       meta: {
         requireAuth: true
       },
-      // children: [
+      children: [
       //   {
       //     path: '/admin/dashboard',
       //     name: 'dashboard',
@@ -40,7 +40,15 @@ export default new Router({
       //       requireAuth: true
       //     }
       //   }
-      // ]
+        {
+          path: '/admin/content/editor',
+          name: 'Editor',
+          component: () => import('../components/admin/content/ArticleEditor'),
+          meta: {
+            requireAuth: true
+          }
+        },
+      ]
     },
     //注意我们并没有把首页的访问路径设置为 /home/index，
     // 仍然可以通过 /index 访问首页，这样配置其实是感受不到 /home 这个路径的存在的。之后再添加新的页面，可以直接在 children 中增添对应的内容。
@@ -67,17 +75,14 @@ export default new Router({
           }
         },
         {
-          path: '/admin/content/editor',
-          name: 'Editor',
-          component: () => import('../components/admin/content/ArticleEditor'),
-          meta: {
-            requireAuth: true
-          }
-        },
-        {
           path: '/jotter',
           name: 'Jotter',
           component: () => import('../components/jotter/Articles')
+        },
+        {
+          path: '/jotter/article',
+          name: 'Article',
+          component: () => import('../components/jotter/ArticleDetails')
         },
       ]
     }
